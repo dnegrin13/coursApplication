c15a6058854c955c4d35ceea80e3e2e739e77bcb
dnegrin13 <dylannegrin13@gmail.com>
2023-09-26 16:22:43 +0200

    modification de l'index.php

--- index.php
+++ index.php
@@ -7,37 +7,50 @@
     <title>Liste des cours</title>
 </head>
 <body>
-    <h1>Liste des cours</h1>
+    <header>
+        <h1>Liste des cours</h1>
+    </header>
+    <div class="contenerRetAjoutlecons">
+            <div class="rechercher">
+                <label for="rechercher">Rechercher</label>
+                <input type="text" name="rechercher" id="rechercher">
+            <div class="AjoutLecons">
+                <a class="lecons" href="form_update_lesson.php">Ajouter une leçon</a>
+            </div>
+            </div>
+        </div>
+            <div class="HTML-CSS">
+                HTML/CSS
+                <?php
+                    include "pdo.php";
 
-    <?php
-        include "pdo.php";
+                    $sql = "SELECT * FROM lessons";
+                    $stmt = $pdo->query($sql);
+                    $lessons = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                    $count = $stmt->rowCount();
+                ?>
+                <ul>
+                    <?php
 
-        $sql = "SELECT * FROM lessons";
-        $stmt = $pdo->query($sql);
-        $lessons = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        $count = $stmt->rowCount();
-    ?>
-
-    <ul>
-        <?php
-
-        if($count > 0){
-            foreach ($lessons as $lesson) {
-                echo 
-                "<li>
-                <a href='lesson.php?id=$lesson[id_lesson]'>$lesson[title]</a>
-                <a href='delete_lesson.php?id=$lesson[id_lesson]'><img src='img/garbage.svg' width='15px'></a>
-                <a href='form_update_lesson.php?id=$lesson[id_lesson]'><img src='img/pencil.svg' width='15px'></a>
-                </li>";
-            }
-        }else{
-            echo "<li>Aucun resultat trouvé.</li>";
-        }
-        ?>
-    </ul>
-
-    <a href="form_update_lesson.php">Ajouter une leçon</a>
-
+                    if($count > 0){
+                        foreach ($lessons as $lesson) {
+                            echo 
+                            "<li>
+                            <a href='lesson.php?id=$lesson[id_lesson]'>$lesson[title]</a>
+                            <a href='delete_lesson.php?id=$lesson[id_lesson]'><img src='img/garbage.svg' width='15px'></a>
+                            <a href='form_update_lesson.php?id=$lesson[id_lesson]'><img src='img/pencil.svg' width='15px'></a>
+                            </li>";
+                        }
+                    }else{
+                        echo "<li>Aucun resultat trouvé.</li>";
+                    }
+                    ?>
+                </ul>
+    </div>
+    <div class="contenerInfoEtHoraire">
+        
+    </div>
+    <footer></footer>    
 
 
 </body>

c662cd117c8536fd2811b87d8d71a462216249e3
dnegrin13 <dylannegrin13@gmail.com>
2023-09-27 16:18:21 +0200

    flex du site fini

--- index.php
+++ index.php
@@ -15,40 +15,54 @@
                 <label for="rechercher">Rechercher</label>
                 <input type="text" name="rechercher" id="rechercher">
             <div class="AjoutLecons">
-                <a class="lecons" href="form_update_lesson.php">Ajouter une leçon</a>
+                <a class="lecons" href="form_add_lesson.php">Ajouter une leçon</a>
             </div>
             </div>
         </div>
-            <div class="HTML-CSS">
-                HTML/CSS
-                <?php
-                    include "pdo.php";
-
-                    $sql = "SELECT * FROM lessons";
-                    $stmt = $pdo->query($sql);
-                    $lessons = $stmt->fetchAll(PDO::FETCH_ASSOC);
-                    $count = $stmt->rowCount();
-                ?>
-                <ul>
+        <div class="contenerLecons">
+            <div class="Cours">    
+                <div class="HTML-CSS">
+                    Resumer de la derniere leçon 1
+                <div class="NomCours">
+                    <a href="lesson.php">Flexbox</a>
+                </div>
+                </div>    
+                <div class="horaire">Resumer de la derniere leçon 2
+                    <div class="NomCours"></div>
+                </div>
+                <div class="information">Resumer de la derniere leçon 3
+                    <div class="NomCours"></div>
+                </div>
+            </div>
+       
+            <div class="sommaire">Sommaire
                     <?php
+                        include "pdo.php";
 
-                    if($count > 0){
-                        foreach ($lessons as $lesson) {
-                            echo 
-                            "<li>
-                            <a href='lesson.php?id=$lesson[id_lesson]'>$lesson[title]</a>
-                            <a href='delete_lesson.php?id=$lesson[id_lesson]'><img src='img/garbage.svg' width='15px'></a>
-                            <a href='form_update_lesson.php?id=$lesson[id_lesson]'><img src='img/pencil.svg' width='15px'></a>
-                            </li>";
-                        }
-                    }else{
-                        echo "<li>Aucun resultat trouvé.</li>";
-                    }
+                        $sql = "SELECT * FROM lessons";
+                        $stmt = $pdo->query($sql);
+                        $lessons = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                        $count = $stmt->rowCount();
                     ?>
-                </ul>
-    </div>
-    <div class="contenerInfoEtHoraire">
-        
+                    <ul>
+                        <?php
+
+                        if($count > 0){
+                            foreach ($lessons as $lesson) {
+                                echo 
+                                "<li>
+                                <a href='lesson.php?id=$lesson[id_lesson]'>$lesson[title]</a>
+                                <a href='delete_lesson.php?id=$lesson[id_lesson]'><img src='img/garbage.svg' width='15px'></a>
+                                <a href='form_update_lesson.php?id=$lesson[id_lesson]'><img src='img/pencil.svg' width='15px'></a>
+                                </li>";
+                            }
+                        }else{
+                            echo "<li>Aucun resultat trouvé.</li>";
+                        }
+                        ?>
+                    </ul>
+            </div>
+        </div>       
     </div>
     <footer></footer>    
 
